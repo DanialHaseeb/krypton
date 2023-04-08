6cc372ba485c6abe30d65e6d715807699f7a3f01
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2023-04-08 17:42:46 +0500

    Made input parser for Enigma analysis

--- src/enigma/analyse.rs
+++ src/enigma/analyse.rs
@@ -1,4 +1,31 @@
+use std::io;
+
+use crate::Γ;
+use crate::Σ;
+use super::key::Key;
+
 pub fn run()
 {
   eprintln!("Enigma analysis ran");
+
+  let input = parse();
+
+
+}
+
+fn parse() -> String
+{
+  let mut input = String::new();
+  while let Some(Ok(line)) = io::stdin().lines().next()
+  {
+    for σ in line.chars()
+    {
+      if !(σ.is_ascii_alphabetic())
+      { continue; }
+
+      let σ = σ.to_ascii_uppercase();
+      input.push(σ);
+    }
+  }
+  input
 }
